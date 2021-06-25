937cb58bf512bf9edf94ca17b07468dab69c3dbe
Atsuya2000 <atuyajyun@gmail.com>
2021-06-25 15:49:47 +0000

    add favarites product

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   get 'chats/show'
   devise_for :users
   root to: 'homes#top'
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :favorites, only: [:create, :destroy]
+  end
+
   get 'chat/:id' => 'chats#show', as: 'chat'
   resources :chats, only: [:create]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_23_042714) do
+ActiveRecord::Schema.define(version: 2021_06_25_153336) do
 
   create_table "chats", force: :cascade do |t|
     t.integer "user_id"
     t.integer "room_id"
     t.string "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
