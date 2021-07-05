b62658ee61769991812f70a7b23f3e72b11768e1
Atsuya2000 <atuyajyun@gmail.com>
2021-07-05 07:31:42 +0000

    fix Article

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController <
     @user = User.find(params[:user_id])
     favorite = current_user.favorites.new(user_id: user.id)
     favorite.save
-    redirect_to user_path(user)
+    redirect_to users_path
   end
 
   def destroy
     @user = User.find(params[:user_id])
     favorite = current_user.favorites.find_by(user_id: user.id)
     favorite.destroy
-    redirect_to user_path(user)
+    redirect_to users_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,11 @@ class User < ApplicationRecord
   attachment :profile_image
 
   has_many :favorites, dependent: :destroy
+
+  def favorited_by?(user)
+    favorites.where(user_id: user.id).exists?
+  end
+
   has_many :user_rooms
   has_many :chats
   has_many :rooms, through: :user_rooms
