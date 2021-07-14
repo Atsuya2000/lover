2ce46f18d2a6b2bf79ed5c116062f60ce35aac0f
Atsuya2000 <atuyajyun@gmail.com>
2021-07-14 11:43:32 +0000

    fix image-size

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '176ce07b97b7ce0a450efed162cc8de21e0deabc9f0fcb821fa29c23f0d1117ee8262767ca67cc0c1fdc2c311c41769585e3130fe121601330602c89d5d515fa'
