ed98ef0c98f0224d91e726e185336c786e2a1a12
Atsuya2000 <atuyajyun@gmail.com>
2021-10-08 14:20:17 +0000

    fix confirm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def index
-    @user = User.page(params[:page]).reverse_order.per(1)
+    @user = User.page(params[:page]).reverse_order.per(5)
   end
 
   def show
